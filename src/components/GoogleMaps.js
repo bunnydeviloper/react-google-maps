0a2723ce4d6e2736a261fe3258579f5d8a8a30c1
Sophia Mai <phuongmai001@gmail.com>
2018-11-28 08:32:55 -0800

    remove map component from react-google-maps

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -1,19 +1,1 @@
 import React from 'react';
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
-
-export const MapWithAMarker = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={10}
-    defaultCenter={{ lat: 47.6205063, lng: -122.3514661 }}
-  >
-    <Marker
-      position={{ lat: 47.6205063, lng: -122.3514661 }}
-    />
-  </GoogleMap>
-));
-
