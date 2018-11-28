06c530cc85bd58a43e432bedaa4cd18565f2baa5
Sophia Mai <phuongmai001@gmail.com>
2018-11-27 22:58:57 -0800

    add template for Google Maps Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import GoogleMaps from './components/GoogleMaps';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <div>
-            TODO: create NavBar component here
-          </div>
-          <div>
-            TODO: create GoogleMap component here
-          </div>
-          <div>
-            TODO: create SidePanel component here
-          </div>
+        <div>
+          TODO: create NavBar component here
+        </div>
+        <GoogleMaps />
+        <div>
+          TODO: create SidePanel component here
+        </div>
       </div>
     );
   }
---
+++ src/components/GoogleMaps.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class GoogleMaps extends Component {
+  render() {
+    return (
+      <div>Hello from Google Map Component</div>
+    );
+  }
+}
