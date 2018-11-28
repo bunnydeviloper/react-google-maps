b4d6f69b6ad546ea3ffad90f428ca08a507649ee
Sophia Mai <phuongmai001@gmail.com>
2018-11-28 08:51:42 -0800

    create map component with google-maps-react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { MapWithAMarker } from './components/GoogleMaps';
+import GoogleApiWrapper from './components/GoogleMaps';
 
 class App extends Component {
   render() {
-    const API = 'AIzaSyCzZvujlnTYKZGkoiQTbFV1Ghr7yM14IEA';
     return (
       <div className="App">
         <div>
           TODO: create NavBar component here
         </div>
-        <MapWithAMarker
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API}&v=3.exp&libraries=geometry,drawing,places`}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
+        <GoogleApiWrapper />
         <div>
           TODO: create SidePanel component here
         </div>
--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -1,1 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+export class MapContainer extends Component {
+  onMapClicked = () => {
+    console.log('do something');
+  }
+
+  render() {
+    const style = {
+      width: '100%',
+      height: '50%'
+    };
+    return (
+        <Map
+          google={this.props.google}
+          style={style}
+          initialCenter={{
+            lat: 47.620569,
+            lng: -122.349420
+          }}
+          zoom={10}
+          onClick={this.onMapClicked}
+        >
+
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+
+              {/*
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+        */}
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyCzZvujlnTYKZGkoiQTbFV1Ghr7yM14IEA')
+})(MapContainer)
+
+// credit: https://github.com/fullstackreact/google-maps-react
