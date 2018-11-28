e7e1c038e7162c537458b861e2497bbf658693ed
Sophia Mai <phuongmai001@gmail.com>
2018-11-27 23:54:20 -0800

    feature: display a map of seatle with a marker

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import GoogleMaps from './components/GoogleMaps';
+import { MapWithAMarker } from './components/GoogleMaps';
 
 class App extends Component {
   render() {
+    const API = 'AIzaSyCzZvujlnTYKZGkoiQTbFV1Ghr7yM14IEA';
     return (
       <div className="App">
         <div>
           TODO: create NavBar component here
         </div>
-        <GoogleMaps />
+        <MapWithAMarker
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API}&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
         <div>
           TODO: create SidePanel component here
         </div>
--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
-export default class GoogleMaps extends Component {
-  render() {
-    return (
-      <div>Hello from Google Map Component</div>
-    );
-  }
-}
+export const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={10}
+    defaultCenter={{ lat: 47.6205063, lng: -122.3514661 }}
+  >
+    <Marker
+      position={{ lat: 47.6205063, lng: -122.3514661 }}
+    />
+  </GoogleMap>
+));
+
