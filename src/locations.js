192a0b719038767469fcd31bc362f712e6744a06
Sophia Mai <phuongmai001@gmail.com>
2018-11-28 18:27:41 -0800

    remove location file

--- src/locations.js
+++
@@ -1,9 +0,0 @@
-export const locations = {
-  "name": "The Space Needle",
-  "street": "400 Broad St",
-  "city": "Seatle",
-  "state": "WA",
-  "zip": "98109",
-  "url": "https://www.spaceneedle.com/",
-  "pos": { "lat": 47.6205063, "lng": -122.3514661 }
-};
