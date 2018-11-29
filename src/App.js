9cc07fb1b38b0425e03e78b1b8f05e5dab8ec94a
Sophia Mai <phuongmai001@gmail.com>
2018-11-29 13:23:35 -0800

    style: add overral styling for project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import './App.css';
 import locations from './data/locations';
 
 import GoogleMaps from './components/GoogleMaps';
+// import SidePanel from './components/SidePanel';
 
 class App extends Component {
   state = {
     lat: 47.6205063,
     lng: -122.3514661,
     zoom: 13,
-    allLocations: locations
+    allLocations: locations,
+    open: false
+  }
+
+  style = {
+    menuButton: {
+      marginLeft: 10,
+      marginRight: 20,
+      position: "absolute",
+      left: 10,
+      top: 10,
+      background: "white",
+      padding: 10,
+    },
+    hide: {
+      display: "none"
+    },
+    header: {
+      marginTop: "0px"
+    }
+  }
+
+  toggleSidePanel = () => {
   }
 
   render() {
