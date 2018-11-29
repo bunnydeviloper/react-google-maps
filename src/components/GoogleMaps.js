4873951586506f54a21d0c6e0ead15a96e5e053f
Sophia Mai <phuongmai001@gmail.com>
2018-11-28 20:14:05 -0800

    feature: update map state and add updateMarkers method

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -5,13 +5,49 @@ const MAP_KEY = "AIzaSyCzZvujlnTYKZGkoiQTbFV1Ghr7yM14IEA";
 
 export class MapDisplay extends Component {
   state = {
-    map: null
+    map: null,
+    markers: [],
+    markerProps: [],
+    activeMarker: null,
+    activeMarkerProps: null,
+    showingInfoWindow: false
   }
 
   componentDidMount = () => {}
 
   onMapReady = (props, map) => {
     this.setState({ map });
+    this.updateMarkers(this.props.locations);
+  }
+
+  updateMarkers = (locations) => {
+    if (!locations) return;
+    // remove any existing markers
+    this.state.markers.forEach(marker => marker.setMap(null));
+
+    let markerProps = [];
+    let markers = locations.map((eachLocation, index) => {
+      let mProps = {
+        key: index,
+        index,
+        name: eachLocation.name,
+        position: eachLocation.pos,
+        url: eachLocation.url
+      }
+      markerProps.push(mProps);
+
+      let marker = new this.props.google.maps.Marker({
+        position: eachLocation.pos,
+        map: this.state.map,
+        animation: this.props.google.maps.Animation.DROP
+      });
+      marker.addListener('click', () => {
+        this.onMarkerClick(mProps, marker, null);
+      });
+      return marker;
+    });
+
+    this.setState({ markers, markerProps });
   }
 
   onMapClicked = (mapProps, map, clickEvent) => {
